e8603c7d58214ac5724fe6bb9a372d70742360d7
Lucius Hebert <luciushebert13@gmail.com>
2024-05-27 15:44:10 -0300

    Melhorias na interface cadastro e login

--- formulario/urls.py
+++ formulario/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from usuarios.views import index
+from usuarios.views import index, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', index, name='index'),
+    path('login/', login, name='login')
 ]
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    
+    
+    def __str__(self):
+        return self.nome
